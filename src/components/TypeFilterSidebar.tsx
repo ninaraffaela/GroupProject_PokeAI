8b52d2ba32aeacb03e5efa42ba0e4be35f40f8e4
ninaraffaela <nina.r.ziegler@gmail.com>
2025-04-11 14:11:09 +0200

    Icons on the FilterTypeButtons

--- src/components/TypeFilterSidebar.tsx
+++ src/components/TypeFilterSidebar.tsx
@@ -1,5 +1,23 @@
 // src/components/TypeFilterSidebar.tsx
 import React from "react";
+import NormalIcon from "../assets/img/normal.png";
+import FirelIcon from "../assets/img/fire.png";
+import WaterIcon from "../assets/img/water.png";
+import GrassIcon from "../assets/img/grass.png";
+import ElectricIcon from "../assets/img/electric.png";
+import IceIcon from "../assets/img/ice.png";
+import FightingIcon from "../assets/img/fighting.png";
+import PoisonIcon from "../assets/img/poison.png";
+import GroundIcon from "../assets/img/ground.png";
+import FlyingIcon from "../assets/img/flying.png";
+import PsychicIcon from "../assets/img/psychic.png";
+import BugIcon from "../assets/img/bug.png";
+import RockIcon from "../assets/img/rock.png";
+import GhostIcon from "../assets/img/ghost.png";
+import DragonIcon from "../assets/img/dragon.png";
+import DarkIcon from "../assets/img/dark.png";
+import SteelIcon from "../assets/img/steel.png";
+import FairyIcon from "../assets/img/fairy.png";
 
 type Props = {
   selectedType: string;
@@ -48,17 +66,42 @@ const typeColors: Record<string, string> = {
   fairy: "bg-pink-300",
 };
 
+const typeIcons: Record<string, string> = {
+  normal: NormalIcon,
+  fire: FirelIcon,
+  water: WaterIcon,
+  grass: GrassIcon,
+  electric: ElectricIcon,
+  ice: IceIcon,
+  fighting: FightingIcon,
+  poison: PoisonIcon,
+  ground: GroundIcon,
+  flying: FlyingIcon,
+  psychic: PsychicIcon,
+  bug: BugIcon,
+  rock: RockIcon,
+  ghost: GhostIcon,
+  dragon: DragonIcon,
+  dark: DarkIcon,
+  steel: SteelIcon,
+  fairy: FairyIcon,
+};
+
 const TypeFilterSidebar: React.FC<Props> = ({ selectedType, onTypeChange }) => {
   return (
-    <div className="grid grid-cols-2 gap-2 mt-10 ">
+    <div className="grid grid-cols-2 gap-3 mt-10 ">
       {types.map((type) => (
         <button
           key={type}
           onClick={() => onTypeChange(type)}
-          className={`px-3 py-1 rounded-full text-white capitalize shadow ${
+          className={`px-3 py-2 flex flex-col justify-center items-center rounded-[20px] text-white capitalize shadow ${
             selectedType === type ? "ring-2 ring-white" : ""
           } ${typeColors[type]}`}
         >
+          {/* {typeIcons[type]}  */}
+          <div className="bg-stone-50 rounded-full w-15 h-15">
+            <img className="object-contain w-full h-full p-2" src={typeIcons[type]} alt="" />
+          </div>
           {type}
         </button>
       ))}
